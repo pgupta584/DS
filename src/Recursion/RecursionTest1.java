9dc63a6dac0707c39328a80fd7e9f39ec4dd2963
pankajgupta <pgupta584@gmail.com>
2019-09-21 13:58:56 +0530

    added

---
+++ src/Recursion/RecursionTest1.java
@@ -0,0 +1,21 @@
+package Recursion;
+
+public class RecursionTest1 
+{
+	static int recursionTest(int n)
+	{
+		if(n<0)
+		{
+			return 0;
+		}
+		else
+		{
+			recursionTest(n-1);
+			System.out.println("Recursion Cycle "+n);
+		}
+			return n;
+	}
+	public static void main(String[] args) {
+		recursionTest(4);
+	}
+}
